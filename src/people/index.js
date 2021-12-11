446a347dd28bc1500419538da92de72f4e3e299f
J Heaton <jackrobertheaton@gmail.com>
2021-12-11 16:00:09 -0500

    Add person photos, styles

---
+++ src/people/index.js
@@ -0,0 +1,19 @@
+import jkr from "./jkr.jpg"
+import mg from "./mg.jpg"
+import ms from "./ms.jpg"
+import rg from "./rg.jpg"
+import rh from "./rh.jpg"
+import rwe from "./rwe.jpg"
+import tsg from "./tsg.jpg"
+import vw from "./vw.jpg"
+
+export {
+    jkr,
+    mg,
+    ms,
+    rg,
+    rh,
+    rwe,
+    tsg,
+    vw
+}
